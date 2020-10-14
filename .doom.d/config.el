d4f6294b2af041d96d322a583682d155c72705cc
Josh Waller <mdx.programs@gmail.com>
2020-10-14 11:39:11 -0500

    change escape sequence for doom

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -35,6 +35,8 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+(setq evil-escape-key-sequence "jf")
+
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
